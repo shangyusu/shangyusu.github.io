1a02d5e25ec9f090f45ac38ddb7690191911bdae
SamSSY <sam7822567@gmail.com>
2020-09-05 12:41:26 +0800

    update

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,7 @@ import Sidebar from '../components/Sidebar';
 import Link from 'gatsby-link';
 import profilePic from './index_photo.png';
 import testPic from './japan.png';
-import resume from './shang-yu_resume_20200815.pdf';
+import resume from './shang-yu_resume_20200904.pdf';
 
 
 class IndexRoute extends React.Component {
